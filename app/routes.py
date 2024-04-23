f28ad8de2df58401a4d8b90a59e9c61ec86d1fd5
liamdembUWA <23639838@student.uwa.edu.au>
2024-04-23 22:49:26 +0800

    header template and page routing

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,19 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/how_it_works')
+def rules():
+    return render_template('rules.html', title='How it works')
+
+@app.route('/packs')
+def packs():
+    return render_template('packs.html', title='Packs')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Log in')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html', title='Sign up')
