0d2d0c23dcc0c6ab28a06688852e5164117eb3ae
Si-yuY <167667780+Si-yuY@users.noreply.github.com>
2024-04-25 22:39:25 +0800

    added user, post, card, and userInteraction database to app, added some comments to base.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+#initalise a database object
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 #Starts forum
@@ -6,10 +12,21 @@ def start_forum():
     forum = Flask(__name__)
     #Configures the forum with key encrypting Cookies and Session data
     forum.config['SECRET_KEY'] = 'This Key Encrypts Cookies and Session Data Of User'
+    forum.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'  #sqlalchemy database is located at f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
     from .routes import routes
     from .auth import auth
 
     forum.register_blueprint(routes, url_prefix='/')
     forum.register_blueprint(auth, url_prefix='/')
 
+    from . import database
+
+    create_database(app)
+
     return forum
+
+def create_database(app):
+    if not path.exists('app/' + DB_NAME):
+        db.create_all(app=app)
