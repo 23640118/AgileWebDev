9849bc3c05de049cc1ce2d42486e91b79363c10c
Si-yuY <167667780+Si-yuY@users.noreply.github.com>
2024-04-26 20:35:54 +0800

    Implemented database

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,27 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+#initalise a database object
+db = SQLAlchemy()
 
 
 #Starts forum
-"""
-!!!Please Read!!!: 
-Code within start_forum() will execute twice within main.py
-Please make sure that code added to start_forum() will run independently from the numbers of times the function is executed.
-"""
 def start_forum():
     forum = Flask(__name__)
     #Configures the forum with key encrypting Cookies and Session data
     forum.config['SECRET_KEY'] = 'This Key Encrypts Cookies and Session Data Of User'
+    forum.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  #sqlalchemy database is located at 'sqlite:///database.db'
+    db.init_app(forum)
+
     from .routes import routes
     from .auth import auth
-    
-    #This links routes.py and auth.py with the forum application
+
     forum.register_blueprint(routes, url_prefix='/')
     forum.register_blueprint(auth, url_prefix='/')
+
+    from . import database
+    #Create db file
+    with forum.app_context():
+        db.create_all()
 
     return forum
---
+++ app/database.py
@@ -0,0 +1,32 @@
+from app import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Post(db.Model):
+    post_id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    message = db.Column(db.String(500))
+    card_traded = db.Column(db.Integer, db.ForeignKey('card.card_id'))
+    card_wanted = db.Column(db.Integer, db.ForeignKey('card.card_id'))
+    completed = db.Column(db.Boolean)
+    
+class User(db.Model, UserMixin):
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(10))
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(20))
+    cards_owned = db.Column(db.String(100))          #all cards a user owns in a comma-separated list
+
+class Card(db.Model):
+    card_id = db.Column(db.Integer, primary_key=True)
+    rarity = db.Column(db.String(9))                #'RARE', 'EPIC', 'LEGENDARY'
+    name = db.Column(db.String(50), unique=True)
+    url = db.Column(db.String(100))
+
+class UserAction(db.Model):
+    action_id = db.Column(db.Integer, primary_key=True)
+    action_type = db.Column(db.String(10))          #'POST_postID', 'EXCHANGE_postID", 'LOGIN', 'LOGOUT','REGISTER'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    
