2bd58e3b995a563f5f65cadb28d83c0dd04f3b81
liamdembUWA <23639838@student.uwa.edu.au>
2024-05-09 23:26:30 +0800

    Trade functionality

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, flash, redirect, url_for, request
-from flask_login import login_required
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_required, current_user
 from .database import Post, User, Card
 from . import db
 from datetime import datetime
@@ -14,6 +14,43 @@ def index():
     posts = Post.query.all()
     return render_template('index.html', title='Home', posts=posts)
 
+@routes.route('/update-post', methods=['POST'])
+def trade():
+    post_id = request.form.get('post_id')   # Post being completed
+    u = current_user                        # User completing
+
+    post = Post.query.get(post_id)
+    trade_uid = post.owner_id
+    trade_user = User.query.get(trade_uid)
+
+    # Confirm the both users have the required cards
+    for card in post.cards_traded:
+        if card not in trade_user.cards:
+            flash('Trade error', 'error')
+            return trade_user.username + " does not have the needed card to complete this trade!"
+    
+    for card in post.cards_wanted:
+        if card not in u.cards:
+            flash('Trade error', 'error')
+            return "You don't have the needed cards to complete this trade!"
+    
+    # Complete the trade
+    for card in post.cards_traded:
+        trade_user.cards.remove(card)
+        u.cards.append(card)
+
+    for card in post.cards_wanted:
+        trade_user.cards.append(card)
+        u.cards.remove(card)
+    
+    # Mark trade as completed
+    post.completed = True
+
+    db.session.commit()
+    
+    flash('Post updated successfully', 'success')
+    return "Congratulations! You've completed the trade!"
+
 @routes.route('/post', methods=['GET','POST'])
 def post():
     cards = Card.query.all()
@@ -22,9 +59,13 @@ def post():
         cards_traded = request.form.getlist('cards_traded')
         cards_wanted = request.form.getlist('cards_wanted')
 
-        u = db.session.get(User, 1)
+        u = current_user
         new_post = Post(owner_id=u.user_id, date=datetime.now(), message=message, completed=False)
 
+        if len(cards_traded) == 0 or len(cards_wanted) == 0:
+            flash('Cannot trade for 0 cards', 'error')
+            return redirect('/post')
+        
         # Add traded cards to the post
         for card_id in cards_traded:
             card = Card.query.get(card_id)
