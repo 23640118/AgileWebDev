6ab2ed40761b380f4e27f6a70358a09d7976e7cb
Harry Prosser <23134051@student.uwa.edu.au>
2024-05-16 23:16:55 +0800

    Add basic pack opening functionality
    Added open_pack.py which creates a new card, added button that generates a new pack to /packs. Does not add cards to user account yet

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,17 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_login import login_required
+import random
+from .database import Card
 
 routes = Blueprint('routes', __name__)
 
-
+def get_random_card():
+    rarity = ['common', 'rare', 'epic', 'legendary']
+    probability = [0.70, 0.15, 0.05, 0.01]
+    chosen_rarity = random.choices(rarity, weights=probability)[0]
+    rarity_list = Card.query.filter_by(rarity = chosen_rarity).all()
+    return random.choice(rarity_list)
+  
 
 @routes.route('/')
 @routes.route('/index')
@@ -20,3 +28,8 @@ def rules():
 @login_required
 def packs():
     return render_template('packs.html', title='Packs')
+
+@routes.route('/open_pack')
+def open_pack():
+    items = [get_random_card() for _ in range(5)]
+    return render_template('open_pack.html', items=items)
