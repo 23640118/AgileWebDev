74a44f261b5e0b1306863ac4ec1d4ff706913382
23640118 <23640118@student.uwa.edu.au>
2024-05-18 05:08:43 +0800

    Countdown functionality of daily packs

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,8 @@ from flask_login import login_required, LoginManager, current_user
 import random
 from typing import cast
 from . import db
+from datetime import datetime, timedelta
+from .database import UserAction
 
 routes = Blueprint('routes', __name__)
 
@@ -31,22 +33,38 @@ def rules():
 @routes.route('/packs')
 @login_required
 def packs():
-    return render_template('packs.html', title='Packs')
+    from .database import User, UserAction
+    user = cast(User, current_user)
+    # Get the time of the last free pack action
+    last_free_pack_action = UserAction.query.filter_by(user_id=user.user_id, action_type='PACK_FREE').order_by(UserAction.date.desc()).first()
+    if last_free_pack_action:
+        time_since_last_pack = datetime.now() - last_free_pack_action.date
+        if time_since_last_pack < timedelta(hours=24):
+            #Calculate the remaining time
+            remaining_time = timedelta(hours=24) - time_since_last_pack
+        else:
+            remaining_time = timedelta(seconds=0)
+    else:
+        remaining_time = timedelta(seconds=0)
+    return render_template('packs.html', title='Packs', remaining_time=remaining_time.total_seconds())
+
 
 @routes.route('/open_pack')
 @login_required
 def open_pack():
-    from .database import User, Card
+    from .database import User
     user = cast(User, current_user)
     items = [get_random_card() for _ in range(5)]
 
+    #Check for cards in database
     if None in items:
-        flash("An error occurred while opening the pack. Please try again.", "error")
+        flash("An error occurred while opening the pack. Please contact administrators.", "error")
         return "None"
     
     for card in items:
         user.cards.append(card)
-
+    new_action = UserAction(action_type = 'PACK_FREE', user_id = current_user.user_id)
+    db.session.add(new_action)
     db.session.commit()
     return render_template('open_pack.html', items=items)
     
