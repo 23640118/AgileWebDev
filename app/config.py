43dd815ca8c8366453769cf423b1fee2db93d87d
Si-yuY <23380883@student.uwa.edu.au>
2024-05-18 16:13:35 +0800

    Set default secret key for test, written basic test for login, logout and register

--- app/config.py
+++ app/config.py
@@ -4,11 +4,13 @@ import os
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
+ 
 
 class DeploymentConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory"
+    SECRET_KEY = 'secret'
     TESTING = True
--- tests/test_post.py
+++ tests/test_post.py
@@ -1,3 +1,4 @@
+# Test the post functionality
 from app import start_forum, db
 from app.config import TestConfig
 import unittest
@@ -6,7 +7,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 def write_db():
     user = User(username = 'test', email = 'test@t.com', password = generate_password_hash('asd'), money = 100)
-    user2 = User(username = 'test', email = 'test@t.com', password = generate_password_hash('asd'), money = 100)
+    user2 = User(username = 'test', email = 'test1@t.com', password = generate_password_hash('asd'), money = 100)
     card = Card(rarity = 'legendary', name = "Blue", url = '/static/img/cards/Blue.png', artist = 'Jon', year = 1998)
     card2 = Card(rarity = 'epic', name = "Songs in the Key of Life", url = '/static/img/cards/Songs in the Key of Life.png', artist = 'Stevie', year = 1999)
     user.cards.append(card)
@@ -23,12 +24,7 @@ class BasicTest(unittest.TestCase):
         db.create_all()
         write_db()
 
-    def tesrDown(self):
+    def tearDown(self):
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
-
-    def test_password_hashing(self):
-        u = User.query.get(1)
-        self.assertFalse(u.check_password('not_the_password'))
-        self.assertTrue(u.check_password('asd'))
